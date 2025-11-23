8c0a1e9cbacb396f61d8af260ad05afdc7514a54
Seizanoko <74529060+Seizanoko@users.noreply.github.com>
2025-11-23 13:26:02 +0100

    Add unit tests for Station entity

---
+++ backend/tests/Unit/Entity/StationTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Unit\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+use App\Entity\Station;
+
+class StationTest extends TestCase
+{
+    public function testDefaults(): void
+    {
+        // actions
+        $station = new Station();
+
+        // assertions
+        $this->assertNull($station->getId(), 'id is null until persisted');
+        $this->assertNull($station->getShortName(), 'no names set by default');
+        $this->assertNull($station->getLongName(), 'no names set by default');
+    }
+
+    public function testSettersAndGetters(): void
+    {
+        // given
+        $shortName = 'TSTA';
+        $longName = 'Test Station';
+        
+        // actions
+        $station = new Station();
+        $station->setShortName($shortName);
+        $station->setLongName($longName);
+
+        // assertions
+        $this->assertSame($shortName, $station->getShortName());
+        $this->assertSame($longName, $station->getLongName());
+    }
+}
